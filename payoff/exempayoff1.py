77824c456faba5a8b6cfab983a3c8c22401df827
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-18 15:27:40 -0300

    Autocommit a cada 15 minutos

--- payoff/exempayoff1.py
+++ payoff/exempayoff1.py
@@ -2,21 +2,20 @@
 
 import opstrat as op
 
-op.single_plotter()
+# op.single_plotter()
 
 
-op.single_plotter(spot=460, strike=460, op_type='p', tr_type='s', op_pr=12.5)
+# op.single_plotter(spot=460, strike=460, op_type='p', tr_type='s', op_pr=12.5)
 
-op.multi_plotter()
+# op.multi_plotter()
 
+# exemplo com petr4
 
-op1={'op_type': 'c', 'strike': 215, 'tr_type': 's', 'op_pr': 7.63}
-op2={'op_type': 'c', 'strike': 220, 'tr_type': 'b', 'op_pr': 5.35}
-op3={'op_type': 'p', 'strike': 210, 'tr_type': 's', 'op_pr': 7.20}
-op4={'op_type': 'p', 'strike': 205, 'tr_type': 'b', 'op_pr': 5.52}
+op1={'op_type': 'c', 'strike': 36.77, 'tr_type': 's', 'op_pr': 0.76}
+op2={'op_type': 'c', 'strike': 37.27, 'tr_type': 'b', 'op_pr': 0.60}
+op3={'op_type': 'p', 'strike': 34.52, 'tr_type': 's', 'op_pr': 0.51}
+op4={'op_type': 'p', 'strike': 34.02, 'tr_type': 'b', 'op_pr': 0.41}
 
 op_list=[op1, op2, op3, op4]
-op.multi_plotter(spot=212.26,spot_range=10, op_list=op_list)
+op.multi_plotter(spot=36.08,spot_range=10, op_list=op_list)
 
-
-op.yf_plotter()
