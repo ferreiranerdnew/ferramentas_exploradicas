24fc2a03f246c1505fa1d30b3fa72269da75a5a3
FerreiraNerd <ferreiranerd0@gmail.com>
2023-12-18 16:57:40 -0300

    Autocommit a cada 15 minutos

--- payoff/exempayoff1.py
+++ payoff/exempayoff1.py
@@ -22,12 +22,3 @@ op_list=[op1, op2, op3, op4]
 op.multi_plotter(spot=35.78,spot_range=10, op_list=op_list, title_1=title_1)
 
 
-# op1={'op_type': 'c', 'strike': 36.77, 'tr_type': 's', 'op_pr': 0.76}
-# op2={'op_type': 'c', 'strike': 37.27, 'tr_type': 'b', 'op_pr': 0.60}
-# # op3={'op_type': 'p', 'strike': 34.52, 'tr_type': 's', 'op_pr': 0.51}
-# # op4={'op_type': 'p', 'strike': 34.02, 'tr_type': 'b', 'op_pr': 0.41}
-
-# title_1 = 'PETR4 IRON 18/12/2023'
-
-# op_list=[op1, op2]
-# op.multi_plotter(spot=35.78,spot_range=10, op_list=op_list, title_1=title_1)
